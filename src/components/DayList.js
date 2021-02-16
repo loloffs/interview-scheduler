3346d4d80f4a9d1edf906aec547e91034a77074e
Luke Oloffs <luke@Lukes-MacBook-Air.local>
2021-02-16 08:49:55 -0800

    Added button and daylist components

---
+++ src/components/DayList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import DayListItem from "components/DayListItem";
+
+
+export default function DayList(props) {
+
+  const days = props.days.map(day => {
+
+    return (
+    <DayListItem 
+    name={day.name} 
+    spots={day.spots} 
+    selected={day.name === props.day}
+    setDay={props.setDay}  
+    />
+    )
+  }) 
+  
+  return (
+    <ul>{days}</ul>
+  )
+}
+
+// Our DayList component will take in three props.
+
+// days:Array a list of day objects (each object includes an id, name, and spots)
+// day:String the currently selected day
+// setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
